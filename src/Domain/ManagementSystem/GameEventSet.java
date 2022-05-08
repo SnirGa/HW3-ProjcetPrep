fdf5f489deb865358e0b92cbdb0b9095a6576284
Mor <morlevi0666@gmail.com>
2022-05-08 19:13:59 +0300

    Changes to GameEventSet

--- src/Domain/ManagementSystem/GameEventSet.java
+++ src/Domain/ManagementSystem/GameEventSet.java
@@ -3,12 +3,46 @@ package Domain.ManagementSystem;
 import java.util.ArrayList;
 
 public class GameEventSet {
-    public Referee getReferee() {
-        //Should return the referee object
-        return new Referee(1,"dsc","csdc","dcsc", "A");
+    private ArrayList<GameEvent> GameEvents;
+    private Referee referee;
+    private Game game;
+
+    public GameEventSet(Referee referee, Game game) {
+        this.referee = referee;
+        this.game = game;
+        this.GameEvents = new ArrayList<>();
     }
+
     public ArrayList<GameEvent> getGameEvents() {
-        //should return the gameEvents (list)
-        return new ArrayList<>();
+        return this.GameEvents;
+    }
+
+    public void setGameEvents(ArrayList<GameEvent> gameEvents) {
+        GameEvents = gameEvents;
+    }
+
+    public Referee getReferee() {
+        return this.referee;
+        //return new Referee(1,"dsc","csdc","dcsc", "A");
+    }
+
+    public void setReferee(Referee referee) {
+        this.referee = referee;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public void addGameEvent(GameEvent gameEvent){
+        GameEvents.add(gameEvent);
+    }
+
+    public void removeGameEvent(GameEvent gameEvent){
+        GameEvents.remove(gameEvent);
     }
 }
