6f3aa80bf5f8d106c699be369d4a5125ed7ddeb1
Mor <morlevi0666@gmail.com>
2022-05-14 18:13:33 +0300

    UC AddRefree to League + TestUnit

--- src/main/java/Domain/Controllers/UnionRepresentiveController.java
+++ src/main/java/Domain/Controllers/UnionRepresentiveController.java
@@ -1,19 +1,33 @@
 package Domain.Controllers;
-
+import DataAccess.UnionRepresentiveMongoDB;
 import Domain.ManagementSystem.*;
 
 public class UnionRepresentiveController extends EnrollledUserController{
-    public void  addGameScore(){
+    UnionRepresentiveMongoDB urMDB;
 
+    public UnionRepresentiveController() {
+        urMDB = UnionRepresentiveMongoDB.getInstance();
+    }
+
+    public void  addGameScore(){
     }
     @Override
     public void showScreen() {
-
     }
 
-    public void addRefTOSL(LeagueSeason leagueSeason, Referee referee) {
+    public boolean addRefTOSL(String league, int year, Referee referee) {
+        leagueSeason = getLeagueBySeason(league, year);
+        if (leagueSeason != null && referee != null){
+            if (leagueSeason.getLstReferee.contains(referee)){
+                return false;
+            }
+            leagueSeason.addReferee(referee);
+            return true;
+        }
+        return false;
     }
 
     public void ApplySchedulingPolicy(LeagueSeason leagueSeason, GameSchedulingPolicy gameSchedulingPolicy) {
     }
+
 }
