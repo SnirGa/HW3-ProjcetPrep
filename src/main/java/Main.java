422839a40cc7ce2de381e5b8e7bbfb7eccd795c4
SnirGa <64706615+SnirGa@users.noreply.github.com>
2022-05-14 17:43:15 +0300

    updates of PlayerDaoMongoDB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,30 @@
+import java.util.Date;
 import java.util.Scanner;
+
+import DataAccess.PlayerDao;
+import DataAccess.PlayerDaoMongoDB;
+import Domain.ManagementSystem.Player;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import static Domain.ManagementSystem.FilledRole.GoalKeeper;
+
 public class Main{
     public static void main(String[] args){
 
-        MongoClient client= MongoClients.create("mongodb+srv://user:user123456user@cluster0.g7msc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-        MongoDatabase db=client.getDatabase("sampleDB");
-        MongoCollection col=db.getCollection("refr");
-        Document sampleDoc=new Document("_id","1").append("name","john smith");
-        col.insertOne(sampleDoc);
+        //MongoClient client= MongoClients.create("mongodb+srv://user:user123456user@cluster0.g7msc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        //MongoDatabase db=client.getDatabase("sampleDB");
+        //MongoCollection col=db.getCollection("refr");
+        //Document sampleDoc=new Document("_id","1").append("name","john smith");
+        //col.insertOne(sampleDoc);
+
+        //
+        PlayerDaoMongoDB playerDaoMongoDB=new PlayerDaoMongoDB();
+        Player player=new Player(1,"Roni123","123456","Roni Ronaldo",new Date(),GoalKeeper);
+        //playerDaoMongoDB.save(player);
+        playerDaoMongoDB.get("Roni123");
     }
 }
