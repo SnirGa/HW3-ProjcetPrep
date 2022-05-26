c23fcd8fa2abd999fe2f4d3f3f800a6ca433619e
SnirGa <64706615+SnirGa@users.noreply.github.com>
2022-05-26 22:54:24 +0300

    Integration testings

--- src/main/java/Service/SystemManagerApplication.java
+++ src/main/java/Service/SystemManagerApplication.java
@@ -6,17 +6,4 @@ public class SystemManagerApplication {
     SystemManageerController systemManageerController=new SystemManageerController();
 
 
-    public void removeUser(){
-        //UC1
-
-
-    }
-
-    public void ComplaintsResponding(){
-        //UC2
-        //TODO
-
-    }
-
-
 }
