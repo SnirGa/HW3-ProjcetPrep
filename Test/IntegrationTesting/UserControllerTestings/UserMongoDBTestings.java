bae23536c9f35995b19b2bee6033f4eb90c4b177
HodayaTakele <hodayat@post.bgu.ac.il>
2022-05-27 19:24:47 +0300

    Finished and added all testing

--- Test/IntegrationTesting/UserControllerTestings/UserMongoDBTestings.java
+++ Test/IntegrationTesting/UserControllerTestings/UserMongoDBTestings.java
@@ -1,38 +1,77 @@
 package IntegrationTesting.UserControllerTestings;
 
-import DataAccess.UserDaoMongoDB;
+import DataAccess.*;
 import Domain.ManagementSystem.*;
-import Service.UserApplication;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserMongoDBTestings {
+    UserDaoMongoDB userDaoMongoDB = UserDaoMongoDB.getInstance();
+
     @Test
-    public void saveUserAndLoadUserAndDelete(){
-        UserDaoMongoDB userDaoMongoDB=UserDaoMongoDB.getInstance();
-        Date date=new Date();
-        Player player=new Player("Roni123","123456","Roni",date, FilledRole.GoalKeeper);
-        userDaoMongoDB.save(player);
-        Player user=(Player) userDaoMongoDB.get(player.getUserName()).get();
-        String pass=user.getPassword();
-        assertEquals(player.getPassword(),pass);
-        userDaoMongoDB.delete(player);
+    public void SaveGetDeleteAllUser() {
+        //Test
+        TeamOwner teamOwner = new TeamOwner("teamOwner", "1234567", "TeamOwnerName");
+        assertTrue(SaveGetDeleteUser(teamOwner));
+        //Test
+        TeamManager teamManager = new TeamManager("teamManger", "123456", "TeamManger", new ArrayList<String>(Arrays.asList("permission1", "permission2", "permission3")));
+        assertTrue(SaveGetDeleteUser(teamManager));
+        //Test
+        Coach coach = new Coach("coach", "123456", "coachName", "mainCoach", "Football Academy");
+        assertTrue(SaveGetDeleteUser(coach));
+        //Test
+        Player player = new Player("player", "123456", "playerName", new Date(), FilledRole.GoalKeeper);
+        assertTrue(SaveGetDeleteUser(player));
+        //Test
+        Referee referee = new Referee("referee", "123456", "refereeName", "mainReferee");
+        assertTrue(SaveGetDeleteUser(referee));
+        //Test
+        UnionRepresentative unionRepresentative = new UnionRepresentative("unionRepresentative", "123456", "URName");
+        assertTrue(SaveGetDeleteUser(unionRepresentative));
+        //Test
+        Fan fan = new Fan("fan", "123456", "fanUser", null);
+        assertTrue(SaveGetDeleteUser(fan));
     }
-    @Test
-    public void updateUser(){
-        UserDaoMongoDB userDaoMongoDB=UserDaoMongoDB.getInstance();
-        Date date=new Date();
-        Player player=new Player("Roni123","123456","Roni",date, FilledRole.GoalKeeper);
-        userDaoMongoDB.save(player);
-        player.setName("Hodaya");
-        userDaoMongoDB.update(player);
-        EnrolledUser user=(EnrolledUser)userDaoMongoDB.get(player.getUserName()).get();
-        assertTrue(user.getName().equals("Hodaya"));
-        assertTrue(userDaoMongoDB.get("Roni").isEmpty());
+
+    public boolean SaveGetDeleteUser(EnrolledUser user) {
+        saveUser(user);
+        if (getUser(user))
+            if (deleteUser(user))
+                return true;
+        return false;
     }
+
+    public void saveUser(EnrolledUser user) {
+        userDaoMongoDB.save(user);
     }
+
+    public boolean getUser(EnrolledUser user) {
+        try {
+            EnrolledUser getUser = (EnrolledUser) userDaoMongoDB.get(user.getUserName()).get();
+            assertNotNull(getUser);
+            assertEquals(user.getUserName(), getUser.getUserName());
+            assertEquals(user.getPassword(), getUser.getPassword());
+            assertEquals(user.getName(), getUser.getName());
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean deleteUser(EnrolledUser user) {
+        try {
+            userDaoMongoDB.delete(user);
+            Optional getUser = userDaoMongoDB.get(user.getUserName());
+            assertTrue(getUser.isEmpty());
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
